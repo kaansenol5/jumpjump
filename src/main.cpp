f8105a909e5283d3b814048616690c124056bf54
Kaan Şenol <kaansnl@icloud.com>
2022-06-18 20:34:41 +0300

    add fps cap

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "EntityComponents/Texture.h"
 #include "EntityComponents/Animations.h"
 #include "EntityComponents/PlayerController.h"
+#include "EntityComponents/Hitbox.h"
+
 int main(int argc, char* argv[]){
     SDL_Init(SDL_INIT_EVERYTHING);
     IMG_Init(IMG_INIT_PNG);
@@ -11,12 +13,22 @@ int main(int argc, char* argv[]){
     TextureManager::init_windowing("test", 1000, 1000);
     bool quit = false;
     ECSManager ecs_manager;
+    const int FPS = 60;
+    const int frame_delay = 1000 / FPS;
+    Uint32 frame_start;
+    int frame_time;
     entt::entity box = ecs_manager.registry.create();
     SDL_Rect box_dimensions = {10, 10, 40, 40}; 
     ecs_manager.registry.emplace<Transform>(box, box_dimensions, true);
     ecs_manager.registry.emplace<PlayerController>(box);
+    SDL_Texture* texture = TextureManager::load_image("assets/player.png");
+    SDL_Rect texture_source_rect = {0, 0, 32, 32};
+    ecs_manager.registry.emplace<Texture>(box, texture, texture_source_rect);
+    ecs_manager.registry.emplace<Animations>(box, 32, 32, 4);
+    ecs_manager.registry.emplace<Hitbox>(box);
+
     while(!quit){
-        TextureManager::clear(0, 0, 0, 255);
+        frame_start = SDL_GetTicks();
         SDL_Event event;
         SDL_PollEvent(&event);
         switch (event.type)
@@ -24,22 +36,17 @@ int main(int argc, char* argv[]){
         case SDL_QUIT:
             quit = true;
             break;
+        
         default:
             break;
         }
-
-
-/*
-        entt::entity man = ecs_manager.registry.create();
-        SDL_Rect man_dimensions = {430, 100, 400, 400};
-        ecs_manager.registry.emplace<Transform>(man, man_dimensions, false);
-        SDL_Texture* texture = TextureManager::load_image("assets/player.png");
-        SDL_Rect texture_source_rect = {0, 0, 32, 32};
-        ecs_manager.registry.emplace<Texture>(man, texture, texture_source_rect);
-        ecs_manager.registry.emplace<Animations>(man, 32, 32, 4);
-*/
+        TextureManager::clear(0, 0, 0, 255);
         ecs_manager.update();
         TextureManager::present();
+        frame_time = SDL_GetTicks() - frame_start;
+        if(frame_delay > frame_time){
+            SDL_Delay(frame_delay - frame_time);
+        }
     }
     return 0;
 }
